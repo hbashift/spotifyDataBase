3d3888665b796628eabb6c263ceb17dd52bc3ecd
hbashift <chenpavel31@gmail.com>
2022-12-16 21:21:17 +0300

    refactored the code and added a check for duplicates. the report is ready

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"db_seminar/generator"
+	"encoding/base64"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -11,11 +12,19 @@ import (
 func main() {
 
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		"localhost", "5432", "postgres", "afimall", "12345", "disable"))
+		"localhost", "5432", "postgres", "testest", "12345", "disable"))
 
 	if err != nil {
 		fmt.Println(err)
+		fmt.Println("хуйхуй")
 	}
+
+	defer func(db *sqlx.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
 
 	err = db.Ping()
 
@@ -24,11 +33,13 @@ func main() {
 	}
 
 	generator.CreateTables(db)
+
 	generator.FillInDataBase(db)
 
-	err = db.Close()
+	generator.GenerateData(db)
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	s := "i am string"
+	se := base64.StdEncoding.EncodeToString([]byte(s))
+	fmt.Println(se)
+	fmt.Println("all tables are created and filled")
 }
